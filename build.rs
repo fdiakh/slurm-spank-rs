153ea7c4a12c861a72858ade3d179fda38cf939d
François Diakhaté <fdiakh@gmail.com>
2023-03-15 18:40:10 +0100

    Vendor bindings for docs.rs build

--- build.rs
+++ build.rs
@@ -2,6 +2,20 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    if env::var("DOCS_RS").is_ok() {
+        // Use pre-generated bindings when building the documentation
+
+        let source_bindings =
+            PathBuf::from(env::var("CARGO_MANIFEST_DIR").expect("cargo manifest dir is empty"))
+                .join("build/bindings.rs");
+        let dest_bindings = out_path.join("bindings.rs");
+
+        std::fs::copy(source_bindings, dest_bindings)
+            .expect("Failed to copy pre-generated bindings");
+        return;
+    }
+
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         // We define spank_option manually to indicate that string pointers are const
@@ -9,7 +23,6 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
