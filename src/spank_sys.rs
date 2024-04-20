4233df8743ed5beb4bcaed59c0143433af9d8644
François Diakhaté <fdiakh@gmail.com>
2024-04-20 08:33:44 +0200

    Drop support for Slurm 20.11

--- src/spank_sys.rs
+++ src/spank_sys.rs
@@ -14,25 +14,3 @@ pub struct spank_option {
 }
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-#[cfg(feature = "slurm_20_11")]
-
-mod slurm_error_compat {
-    use super::*;
-
-    pub const ESPANK_SUCCESS: u32 = spank_err_ESPANK_SUCCESS;
-    pub const slurm_err_t_ESPANK_ERROR: u32 = spank_err_ESPANK_ERROR;
-    pub const slurm_err_t_ESPANK_BAD_ARG: u32 = spank_err_ESPANK_BAD_ARG;
-    pub const slurm_err_t_ESPANK_NOT_TASK: u32 = spank_err_ESPANK_NOT_TASK;
-    pub const slurm_err_t_ESPANK_ENV_EXISTS: u32 = spank_err_ESPANK_ENV_EXISTS;
-    pub const slurm_err_t_ESPANK_ENV_NOEXIST: u32 = spank_err_ESPANK_ENV_NOEXIST;
-    pub const slurm_err_t_ESPANK_NOSPACE: u32 = spank_err_ESPANK_NOSPACE;
-    pub const slurm_err_t_ESPANK_NOT_REMOTE: u32 = spank_err_ESPANK_NOT_REMOTE;
-    pub const slurm_err_t_ESPANK_NOEXIST: u32 = spank_err_ESPANK_NOEXIST;
-    pub const slurm_err_t_ESPANK_NOT_EXECD: u32 = spank_err_ESPANK_NOT_EXECD;
-    pub const slurm_err_t_ESPANK_NOT_AVAIL: u32 = spank_err_ESPANK_NOT_AVAIL;
-    pub const slurm_err_t_ESPANK_NOT_LOCAL: u32 = spank_err_ESPANK_NOT_LOCAL;
-}
-
-#[cfg(feature = "slurm_20_11")]
-pub use slurm_error_compat::*;
