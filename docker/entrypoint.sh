cc0269243fbc70f6d49709f4628e6e4073acbeef
François Diakhaté <fdiakh@gmail.com>
2025-11-15 10:27:04 +0100

    chore: update integration tests to Slurm 25.11

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 mkdir /sys/fs/cgroup/leaf
 pids=$(</sys/fs/cgroup/cgroup.procs)
 echo "$pids" > /sys/fs/cgroup/leaf/cgroup.procs
-echo '+cpuset +cpu +io +memory +hugetlb +pids +rdma +misc' > /sys/fs/cgroup/cgroup.subtree_control
+echo '+cpuset +cpu +io +memory +pids' > /sys/fs/cgroup/cgroup.subtree_control
 mkdir /sys/fs/cgroup/system.slice/
 sed -i "s!CPUs=1!CPUs=$(nproc) RealMemory=100!" /etc/slurm/slurm.conf
 echo "FirstJobId=100000" >> /etc/slurm/slurm.conf
@@ -11,11 +11,13 @@ echo "joe:x:2000:2000:/tmp/:/bin/bash" >> /etc/passwd
 echo "ibm:x:4000:joe" >> /etc/group
 
 su -s /bin/sh munge -c munged & slurmctld -D >&/dev/null & valgrind -q --log-file=/tmp/valgrind.log slurmd -D -L /tmp/slurmd.log >&/dev/null &
-timeout 10 bash -c "while ! timeout 1 sinfo -t idle | grep -q localhost; do sleep 0.5; echo retry wait; done; sinfo" && "$@"
+timeout 10 bash -c "while ! timeout 1 sinfo -t idle | grep -q localhost; do sleep 0.5; done; sinfo > /dev/null" && "$@"
 ret=$?
 scontrol shutdown
 while pidof slurmd; do sleep 1; done
 
+echo
+echo "===== debug logs ====="
 if [[ -s  /tmp/valgrind.log ]]; then
     echo "Valgrind error"
     cat /tmp/valgrind.log
